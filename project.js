e6a29e92996d06a72dbb06ed61fdb94ca59e5a21
snkreddy10 <s.naveenreddy0796@gmail.com>
2024-05-15 12:35:55 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,4 @@
+document.getElementById('toggleSidebar').addEventListener('click', function() {
+    var sidebar = document.querySelector('.side-bar');
+    sidebar.classList.toggle('sidebar-open');
+  });
